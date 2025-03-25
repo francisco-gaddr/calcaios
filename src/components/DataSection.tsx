2fd5644e45e41204d5a521f94e0c51d343852318
ahmedmohd957 <ahmed.mohd957@gmail.com>
2025-03-25 05:34:20 +0100

    feat: Implement responsive ui for dashboard

--- src/components/DataSection.tsx
+++ src/components/DataSection.tsx
@@ -10,18 +10,18 @@ interface DataSectionProps {
 export default function DataSection({ title }: DataSectionProps) {
   return (
     <section>
-      <h2 className="text-2xl font-bold mb-4">{title}</h2>
+      <h2 className="text-xl sm:text-2xl font-bold mb-4">{title}</h2>
       <div className="bg-lightGray100 shadow-lg shadow-gray-200 p-4 lg:p-6 rounded-lg">
         <div className="mb-4 flex flex-col gap-1">
-          <p className=" font-bold text-md lg:text-2xl mb-1">Input Data</p>
+          <p className="font-bold text-md lg:text-xl mb-1">Input Data</p>
           <p>Such as CSV, XML, PDF, API</p>
         </div>
-        <div className="flex space-x-4 mb-2">
-          <Button size="lg" className="font-semibold">
+        <div className="flex flex-wrap gap-3 mb-2 w-full">
+          <Button className="font-semibold">
             Upload a file
             <ChevronRight />
           </Button>
-          <Button size="lg" variant="outline" className="font-semibold">
+          <Button variant="outline" className="font-semibold">
             Input financial details
             <ChevronRight />
           </Button>
